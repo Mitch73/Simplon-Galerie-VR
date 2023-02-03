befd34f5449ce85e0faf644c9a084d2d71f58689
Myia74 <myiayun74@gmail.com>
2023-02-03 16:36:46 +0100

    Sprite area et sprite Anchor

--- Assets/Scripts/TeleportationManagement.cs
+++ Assets/Scripts/TeleportationManagement.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TeleportationManagement : MonoBehaviour
 {
+    [SerializeField] private InputActionAsset actionAsset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var activate = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Activate");
     }
 
     // Update is called once per frame
